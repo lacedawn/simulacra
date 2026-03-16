db07d857bc737a63694a33a0dbfedbf9a19ac7f1
lacedawn <lenaowo727@gmail.com>
2026-03-16 09:06:43 +0300

    basepath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/simulacra',
   images: {
     unoptimized: true,
     remotePatterns: [
