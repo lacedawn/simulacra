002e7009c2b1607f749b63930c9b03e9feaf6bbb
lacedawn <lenaowo727@gmail.com>
2026-03-17 01:34:22 +0300

    update album links

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -5,37 +5,37 @@ const rawAlbums = [
     {
         title: "Adrenaline",
         artist: "Deftones",
-        spotifyUrl: "https://open.spotify.com/album/4cT0ODkZJm7C0b16xL3R6c",
+        spotifyUrl: "https://open.spotify.com/album/30YN03R4Bjl8Qwz8wPRoXG",
         cover: "https://tse2.mm.bing.net/th?q=Deftones+Adrenaline+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "Gore",
         artist: "Deftones",
-        spotifyUrl: "https://open.spotify.com/album/3KBIF2f7dCxKAdR65k7FFh",
+        spotifyUrl: "https://open.spotify.com/album/3tsXyEbUQehXPaRFCS8K1n",
         cover: "https://tse2.mm.bing.net/th?q=Deftones+Gore+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "Loveless",
         artist: "My Bloody Valentine",
-        spotifyUrl: "https://open.spotify.com/album/3USQKOw0se5pBNbEgpehgY",
+        spotifyUrl: "https://open.spotify.com/album/3USQKOw0se5pBNEndu82Rb",
         cover: "https://tse2.mm.bing.net/th?q=My+Bloody+Valentine+Loveless+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "Deathconsciousness",
         artist: "Have a Nice Life",
-        spotifyUrl: "https://open.spotify.com/album/00eiw4KOJR4mCTvJKxhCnJ",
+        spotifyUrl: "https://open.spotify.com/album/6MH3CAXp8AN8ELrbex18dM",
         cover: "https://tse2.mm.bing.net/th?q=Have+a+Nice+Life+Deathconsciousness+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "A Thousand Things I Could Tell You",
         artist: "Xxtarlit⚸",
-        spotifyUrl: "https://open.spotify.com/search/A%20Thousand%20Things%20I%20Could%20Tell%20You%20Xxtarlit",
+        spotifyUrl: "https://open.spotify.com/album/3IreEWEWvL99tFkoCUT2zy",
         cover: "https://tse2.mm.bing.net/th?q=Xxtarlit+A+Thousand+Things+I+Could+Tell+You+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "Among My Swan",
         artist: "Mazzy Star",
-        spotifyUrl: "https://open.spotify.com/search/Among%20My%20Swan%20Mazzy%20Star",
+        spotifyUrl: "https://open.spotify.com/album/4mhpbf7jYc0L1nBceoebIE",
         cover: "https://tse2.mm.bing.net/th?q=Mazzy+Star+Among+My+Swan+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
@@ -45,21 +45,15 @@ const rawAlbums = [
         cover: "https://tse2.mm.bing.net/th?q=Whirr+Distressor+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
-        title: "Giles Corey",
-        artist: "Giles Corey",
-        spotifyUrl: "https://open.spotify.com/album/4YZxbPrLuMwLZre7JhmdUR",
-        cover: "https://tse2.mm.bing.net/th?q=Giles+Corey+Giles+Corey+album+cover&w=400&h=400&c=7&rs=1&p=0",
-    },
-    {
         title: "we had good times together, don't forget that",
         artist: "Sewerslvt",
-        spotifyUrl: "https://open.spotify.com/search/we%20had%20good%20times%20together%20Sewerslvt",
+        spotifyUrl: "https://open.spotify.com/album/6Hs9pYq5ionwBpGAp6iqi9",
         cover: "https://tse2.mm.bing.net/th?q=Sewerslvt+we+had+good+times+together+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
     {
         title: "Fake It Flowers",
         artist: "beabadoobee",
-        spotifyUrl: "https://open.spotify.com/album/6d1BhnjY8FfJb7e9Z0Qy6U",
+        spotifyUrl: "https://open.spotify.com/album/3SGFxGF2loXeOFZtKvdmxo",
         cover: "https://tse2.mm.bing.net/th?q=beabadoobee+Fake+It+Flowers+album+cover&w=400&h=400&c=7&rs=1&p=0",
     },
 ];
